ae18645c47b941ea9808c9f08d695f6c642aaa35
wirthlin <wirthlin@byu.edu>
2024-08-30 16:59:17 -0600

    spi_cntrl assignment checker

---
+++ spi_cntrl/assignment_check.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+
+# Manages file paths
+import pathlib
+import sys
+
+# Add to the system path the "resources" directory relative to the script that was run
+resources_path = pathlib.Path(__file__).resolve().parent.parent  / 'resources'
+sys.path.append( str(resources_path) )
+
+import test_suite_520
+
+def main():
+
+    tester = test_suite_520.build_test_suite_520("spi_cntrl",  min_err_commits = 6, max_repo_files = 20)
+    tester.add_make_test("sim_spi_cntrl")
+    tester.add_make_test("sim_spi_cntrl_100")
+    tester.add_make_test("synth_spi_cntrl")
+    tester.add_make_test("sim_adxl362")
+    tester.add_make_test("synth_adxl362_cntrl")
+    tester.run_tests()
+
+if __name__ == "__main__":
+    main()
