230a1a085c97e023220fda7de0234c536407011e
Mike Wirthlin <wirthlin@byu.edu>
2024-08-30 15:23:16 -0600

    Mixed repo_test bugs

--- resources/repo_test_suite.py
+++ resources/repo_test_suite.py
@@ -107,10 +107,11 @@ class repo_test_suite():
 
     def print_test_start_message(self):
         """ Start message at start of test """
-        self.print_test_status(f"Running test {self.test_name}")
+        self.print_test_status(f"Running test \'{self.test_name}\'")
+        self.print_test_status()
 
     def print_test_end_message(self):
-        self.print_test_status(f"Test completed")
+        self.print_test_status(f"Test completed \'{self.test_name}\'")
         warnings = []
         errors = []
         success = []
